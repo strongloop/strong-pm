c895a5c2468e30c52a3c03df5d5393717fe61953
Sam Roberts <sam@strongloop.com>
2014-06-10 16:41:40 -0400

    prepare: configurable commands to prepare app

---
+++ test/test-prepare.js
@@ -0,0 +1,61 @@
+var assert = require('assert');
+var async = require('async');
+var debug = require('debug')('strong-deploy:test');
+
+console.log('working dir for %s is %s', process.argv[1], process.cwd());
+
+var prepare = require('../lib/prepare').prepare;
+
+// Check for node silently exiting with code 0 when tests have not passed.
+var ok = false;
+
+process.on('exit', function(code) {
+  if (code === 0) {
+    assert(ok);
+  }
+});
+
+function test(config) {
+  return function(callback) {
+    console.log('test config: %j', config);
+    var commit = {
+      config: {
+        prepare: config,
+      },
+      hash: 'HASH',
+      commands: [],
+      spawn: function(cmd, options) {
+        assert.equal(options.env, process.env);
+        assert.equal(options.stdio, 'inherit');
+        this.commands.push(cmd);
+        return this;
+      },
+      on: function(event, callback) {
+        assert.equal(event, 'exit');
+        process.nextTick(function() {
+          callback(0);
+        });
+      },
+    };
+
+    prepare(commit, function(err) {
+      console.log('commands: %j', err || commit.commands);
+      assert.ifError(err);
+      assert.deepEqual(config, commit.commands);
+      return callback();
+    });
+  };
+}
+
+async.series([
+  test(require('../lib/config').configDefaults.prepare),
+  test([]),
+  test(['some command']),
+  test(['some command', 'some other command', 'an another']),
+], function(er, results) {
+  debug('error=%s:', er, results);
+  assert.ifError(er);
+  ok = true;
+});
+
+ok = true;
