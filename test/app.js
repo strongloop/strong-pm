b8df364b5e1bc1869a8398c9de9d3a18b5514f97
Sam Roberts <sam@strongloop.com>
2014-06-10 19:00:29 -0400

    receive: test git receive against test/app

---
+++ test/app.js
@@ -0,0 +1,75 @@
+var assert = require('assert');
+var async = require('async');
+var debug = require('debug')('strong-deploy:test');
+var path = require('path');
+var util = require('util');
+
+require('shelljs/global');
+
+function ex(cmd, async) {
+  console.log('exec `%s`', cmd);
+  return exec(cmd, async);
+}
+
+exports.ex = ex;
+
+console.log('working dir for %s is %s', process.argv[1], process.cwd());
+
+var receive = require('../lib/receive').listen;
+
+// Check for node silently exiting with code 0 when tests have not passed.
+exports.ok = false;
+
+process.on('exit', function(code) {
+  if (code === 0) {
+    assert(exports.ok);
+  }
+});
+
+function package() {
+  return require(path.resolve(pwd(),'package.json'));
+}
+
+exports.package = package;
+
+cd('./app'); // tap runs tests from ./test/, you should do likewise
+
+var APPNAME = 'test-app';
+exports.APPNAME = APPNAME;
+
+assert.equal(package().name, APPNAME, 'cwd is ' + APPNAME);
+
+rm('-rf', '../receive-base');
+rm('-rf', '.git');
+ex('git clean -f -d -x .');
+assert(!test('-e', 'node_modules'));
+ex('git init');
+ex('git add .');
+ex('git ci -m initial');
+ex('sl-build --install --commit');
+
+assert(!test('-e', 'node_modules/debug'), 'dev dep not installed');
+assert(test('-e', 'node_modules/node-syslog'), 'prod dep installed');
+assert(!test('-e', 'node_modules/node-syslog/build'), 'addons not built');
+
+console.log('test/app built succesfully');
+
+var server;
+var port;
+
+exports.listen = function() {
+  server = receive(0, '../receive-base');
+  server.on('listening', function() {
+    port = this.address().port;
+    console.log('git receive listening on  %d', port);
+  });
+  return server;
+};
+
+exports.push = function(repo) {
+  var cmd = util.format('git push http://127.0.0.1:%d/%s', port, repo);
+  // Must be async... or we block ourselves from receiving
+  ex(cmd, function() {
+    console.log('async pushed...');
+  });
+};
---
+++ test/test-receive.js
@@ -0,0 +1,22 @@
+var app = require('./app');
+var assert = require('assert');
+var path = require('path');
+
+var server = app.listen();
+
+var REPO = 'some-repo-name';
+
+server.on('listening', function() {
+  app.push(REPO);
+});
+
+server.on('commit', function(commit) {
+  this.close();
+
+  assert.equal(
+    require(path.resolve(commit.dir, 'package.json')).name,
+    app.APPNAME);
+  assert.equal(commit.repo, REPO);
+  console.log('PASS');
+  app.ok = true;
+});
