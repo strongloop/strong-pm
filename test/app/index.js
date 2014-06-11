354e4294f0bd6bc1daee29d8857c3f879311b1da
Sam Roberts <sam@strongloop.com>
2014-06-11 14:29:13 -0400

    test: app reports SIGINT/TERM/HUP

--- test/app/index.js
+++ test/app/index.js
@@ -1,5 +1,18 @@
 require('node-syslog');
 
-require('http').createServer().listen(process.env.PORT || 0, function() {
-  console.log('listening on %s', this.address().port);
+var server = require('http').createServer().listen(process.env.PORT || 0, function() {
+  console.log('pid %d listening on %s', process.pid, this.address().port);
 });
+
+function handler(signame) {
+  console.log('die on %s', signame);
+  process.kill(process.pid, signame);
+}
+
+function exitOn(signame) {
+  process.once(signame, handler.bind(null, signame));
+}
+
+exitOn('SIGTERM');
+exitOn('SIGINT');
+exitOn('SIGHUP');
