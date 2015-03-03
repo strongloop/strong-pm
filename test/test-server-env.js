0d479b8d1deb4a5908b54e8e1c2dbfe231d60fb7
Ryan Graham <ryan@strongloop.com>
2015-03-02 19:29:01 -0800

    server: Use/expose env provided by Service model

---
+++ test/test-server-env.js
@@ -0,0 +1,54 @@
+var Server = require('../lib/server');
+var mktmpdir = require('mktmpdir');
+var test = require('tap').test;
+
+test('new server', function(t) {
+  mktmpdir(function(err, tmpdir, cleanup) {
+    t.on('end', cleanup);
+
+    var s = new Server('pm', tmpdir, 0, null);
+    var Service = s._app.models.ServerService;
+    var svc = null;
+
+    var emptyEnv = {};
+    var fullEnv = {FOO: 'foo', BAR: 'bar'};
+
+    t.test('start server', function(tt) {
+      s.start(function(err) {
+        tt.ifError(err);
+        tt.end();
+      });
+    });
+
+    t.test('initial environment', function(tt) {
+      Service.findById(1, function(err, _svc) {
+        tt.ifError(err, 'finds Service instance');
+        svc = _svc;
+        tt.equivalent(svc.env, emptyEnv, 'model env is empty');
+        tt.equivalent(s.env({}), emptyEnv, 'local env is empty');
+        tt.end();
+      });
+    });
+
+    t.test('update env on model', function(tt) {
+      svc.env = fullEnv;
+      svc.save(function(err, res) {
+        tt.ifError(err, 'saves without error');
+        tt.equivalent(res.env, fullEnv, 'saved env on model');
+        tt.end();
+      });
+    });
+
+    t.test('update reflected locally', function(tt) {
+      tt.equivalent(s.env({}), fullEnv, 'local env was updated');
+      tt.end();
+    });
+
+    t.test('shutdown server', function(tt) {
+      s.stop(function(err) {
+        tt.ifError(err, 'server shutdown without error');
+        tt.end();
+      });
+    });
+  });
+});
