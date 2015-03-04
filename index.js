f9cc01ec5613365493c33d9e9fc018ba7dcfb155
Sam Roberts <sam@strongloop.com>
2015-03-04 09:21:21 -0800

    pm: remove duplicate check for parent-death

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// Exit on loss of parent process, if it had established an ipc control channel.
-// We do this ASAP because we don't want child processes to leak, outliving
-// their parent. If the parent has not established an 'ipc' channel to us, this
-// will be a no-op, the disconnect event will never occur.
-process.on('disconnect', function() {
-  process.exit(2);
-});
+'use strict';
 
 var Parser = require('posix-getopt').BasicParser;
 var assert = require('assert');
