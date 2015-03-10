f2e6e793196551d91e83b93ae4c1f0dd43aa3855
braincomb <zapper@gmail.com>
2015-03-10 12:36:12 -0700

    Pass description option when installing service

--- test/test-install-systemd.sh
+++ test/test-install-systemd.sh
@@ -18,6 +18,7 @@ assert_exit 0 $CMD --port 7777 \
 # Simple lines that should be in the service file for this config
 assert_file $TMP/systemd.service "ExecStart=$(node -p process.execPath)"
 assert_file $TMP/systemd.service "WorkingDirectory=$HOME"
+assert_file $TMP/systemd.service "Description=StrongLoop Process Manager"
 
 unset SL_PM_INSTALL_IGNORE_PLATFORM
 assert_report
