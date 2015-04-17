91b52a2fce310d5f48a19330bcc5ab62aa568883
Sam Roberts <sam@strongloop.com>
2015-04-17 08:59:50 -0700

    test-config rm, config is gone

--- test/test-config.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-
-var config = require('../lib/config');
-var configForCommit = config.configForCommit;
-var configDefaults = config.configDefaults;
-var expected = JSON.parse(JSON.stringify(config.configDefaults));
-expected.configFile = 'CONFIGFILE.ignored';
-assert.deepEqual(configForCommit('CONFIGFILE'), expected);
