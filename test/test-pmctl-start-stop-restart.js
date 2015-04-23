4c701e1200e4a927b2f10e89fc126237d6eb4c2b
Sam Roberts <sam@strongloop.com>
2015-04-22 21:23:08 -0700

    test: pmctl-start stop using get-process-count
    Still failing because dead processes are in status and count.

--- test/test-pmctl-start-stop-restart.js
+++ test/test-pmctl-start-stop-restart.js
@@ -4,37 +4,38 @@ var helper = require('./helper-pmctl');
 helper.test('pmctl', function(t, pm) {
   var pmctl = pm.pmctlFn;
 
-  t.waiton(pmctl('status'), 'Processes');
+  t.waiton(pmctl('get-process-count', '1'), 'processes: 1');
 
   t.test('start/restart/resize', function(t) {
     t.expect(pmctl('set-size', '1', '0'));
-    t.waiton(pmctl('status'), 'Processes');
+    t.waiton(pmctl('get-process-count', '1'), 'processes: 1');
 
     t.failon(pmctl('start', '1'), 'running, so cannot be started');
     t.expect(pmctl('stop', '1'), 'Service.*hard stopped');
     t.waiton(pmctl('status'), 'Not started');
+
     // XXX(sam) this test fails, hard stop succeeds silently when service
     // is already stopped
     // t.failon(pmctl('stop', '1'), 'not running, so cannot be stopped');
+
     t.expect(pmctl('start', '1'), 'starting');
-    t.waiton(pmctl('status', '1'), 'Processes');
+    t.waiton(pmctl('get-process-count', '1'), 'processes: 1');
+
     t.expect(pmctl('restart', '1'), 'Service.*restarting');
-    t.waiton(pmctl('status', '1'), 'Processes');
 
-    // XXX(sam) rest of test needs get-worker-count
-    return;
+    t.waiton(pmctl('get-process-count', '1'), 'processes: 1');
 
-    t.waiton(pmctl('status', '1'), /worker count: *0/);
     t.expect(pmctl('set-size', '1', '3'));
-    t.waiton(pmctl('status', '1'), /worker count: *3/);
-    t.expect(pmctl('soft-restart', '1'), 'stopped with status 0, restarting');
-    t.waiton(pmctl('status', '1'), /worker count: *0/);
+    t.waiton(pmctl('get-process-count', '1'), /processes: 4/);
 
-    t.expect(pmctl('set-size', '1'));
-    t.waiton(pmctl('status'), /worker count: *1/);
+    t.expect(pmctl('soft-restart', '1'), 'Service.*soft restarting');
+    t.waiton(pmctl('get-process-count', '1'), /processes: 1/);
 
-    t.waiton(pmctl('restart', '1'), 'stopped with status SIGTERM, restarting');
-    t.waiton(pmctl('status'), /worker count: *0/);
+    t.expect(pmctl('set-size', '1', '1'));
+    t.waiton(pmctl('get-process-count', '1'), /processes: 2/);
+
+    t.expect(pmctl('restart', '1'), 'Service.*restarting');
+    t.waiton(pmctl('get-process-count', '1'), /processes: 1/);
   });
 
   t.shutdown(pm);
