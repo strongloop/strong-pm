39ee134dcb4103d6eb76f29cb68b663eb8f22e09
Sam Roberts <sam@strongloop.com>
2015-04-24 15:03:32 -0700

    test: info test working against new command

--- test/test-pmctl-info.js
+++ test/test-pmctl-info.js
@@ -1,18 +1,16 @@
 var fmt = require('util').format;
 var helper = require('./helper-pmctl');
-
-require('tap').test('pmctl info', {skip: 'unimplemented yet'}, function() {
-});
-return;
+var version = require('../package').version;
 
 helper.test('pmctl', function(t, pm) {
   var pmctl = pm.pmctlFn;
 
-  t.waiton(pmctl('status'), /Processes:$/m);
+  t.waiton(pmctl('status', '1'), /Processes:$/m);
 
-  /* XXX(sam) pm PID/port is no longer available? */
-  t.test('status has pm pid', function(t) {
-    t.expect(pmctl('status'), fmt('pid: *%d', pm.pid));
+  t.test('info', function(t) {
+    t.expect(pmctl('info'), fmt('Version *%s', version));
+    t.expect(pmctl('info'), fmt('PID *%d', pm.pid));
+    t.expect(pmctl('info'), fmt('Port *%d', pm.port));
   });
 
   t.shutdown(pm);
