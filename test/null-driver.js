31096518a31ba787da1fa2397e3c6981322e02c9
Ryan Graham <ryan@strongloop.com>
2015-04-29 14:04:54 -0700

    test: unit tests for ctl handlers

---
+++ test/null-driver.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var assert = require('assert');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+module.exports = exports = NullDriver;
+
+function NullDriver(options) {
+  if (!(this instanceof NullDriver)) {
+    return new NullDriver(options);
+  }
+  assert('baseDir' in options, 'baseDir is provided');
+  assert('console' in options, 'console is provided');
+  assert('server' in options, 'server is provided');
+  EventEmitter.call(this);
+}
+
+util.inherits(NullDriver, EventEmitter);
+
+exports.type = 'Null';
+
+NullDriver.prototype.setStartOptions = function(id, opts) {
+  assert(id, 'id is provided');
+  assert(opts, 'opts is provided');
+  return;
+};
+
+NullDriver.prototype.removeInstance = function(id, cb) {
+  assert(id, 'id is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.deployInstance = function(id, req, res) {
+  assert(id, 'id is provided');
+  assert(req, 'req is provided');
+  assert(res, 'res is provided');
+  return;
+};
+
+NullDriver.prototype.startInstance = function(id, cb) {
+  assert(id, 'id is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.stopInstance = function(id, style, cb) {
+  assert(id, 'id is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.dumpInstanceLog = function(id) {
+  assert(id, 'id is provided');
+  return;
+};
+
+NullDriver.prototype.updateInstanceEnv = function(id, env, cb) {
+  assert(id, 'id is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.requestOfInstance = function(id, req, cb) {
+  assert(id, 'id is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.start = function(meta, cb) {
+  assert(meta, 'meta is provided');
+  setImmediate(cb);
+};
+
+NullDriver.prototype.stop = function(cb) {
+  setImmediate(cb);
+};
+
+NullDriver.prototype._containerById = function(id) {
+  assert(id, 'id is provided');
+  return new EventEmitter();
+};
+
+NullDriver.prototype.getName = function() {
+  return 'Null';
+};
+
+NullDriver.prototype.getStatus = function() {
+  return 'running';
+};
---
+++ test/test-ctl.js
@@ -0,0 +1,94 @@
+var tap = require('tap');
+var ctl = require('../lib/ctl');
+var Server = require('../lib/server');
+var util = require('util');
+var ServiceManager = require('../lib/service-manager');
+var NullDriver = require('./null-driver');
+
+tap.test('shutdown', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('start', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('stop', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('soft-stop', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('restart', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('soft-restart', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('env-set', function(t) {
+  t.plan(4);
+
+  function MockServer() {
+    this._driver = new MockDriver(this);
+  }
+  util.inherits(MockServer, Server);
+
+  function MockDriver(server) {
+    this.server = server;
+  }
+  util.inherits(MockDriver, NullDriver);
+
+  t.assert(NullDriver.prototype.updateInstanceEnv, 'Driver method exists');
+  MockDriver.prototype.updateInstanceEnv = function(id, env, callback) {
+    t.equal(id, '1', 'defaults to instance id 1');
+    setImmediate(callback, null, env);
+  };
+
+  var opts = {server: new MockServer()};
+  var req = {cmd: 'env-set', env: {foo: 'bar'}};
+  ctl(opts, req, function(rsp) {
+    t.assert(!rsp.error, 'should not error');
+    t.assert(rsp.message, 'should have a message')
+    t.end();
+  });
+});
+
+tap.test('env-get', function(t) {
+  t.plan(4);
+
+  function MockServer() {
+    this._serviceManager = new MockServiceManager(this);
+  }
+  util.inherits(MockServer, Server);
+
+  function MockServiceManager(server) {
+    this.server = server;
+  }
+
+  t.assert(ServiceManager.prototype.getInstanceEnv,
+           'ServiceManager method exists');
+  MockServiceManager.prototype.getInstanceEnv = function(id, callback) {
+    t.equal(id, '1', 'defaults to instance id 1');
+    setImmediate(callback, null, {});
+  };
+
+  var opts = {server: new MockServer()};
+  var req = {cmd: 'env-get'};
+  ctl(opts, req, function(rsp) {
+    t.assert(!rsp.error, 'should not error');
+    t.assert(rsp.env, 'should have an env')
+    t.end();
+  });
+});
+
+tap.test('log-dump', {todo: true}, function(t) {
+  t.end();
+});
+
+tap.test('current', {todo: true}, function(t) {
+  t.end();
+});
