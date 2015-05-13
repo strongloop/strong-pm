eba8af25ffaa607883cb199406de9d806110410c
Ryan Graham <ryan@strongloop.com>
2015-05-12 19:58:45 -0700

    test: make test-pmctl-rest-digest-auth tap@1 safe

--- test/test-pmctl-rest-digest-auth.js
+++ test/test-pmctl-rest-digest-auth.js
@@ -1,4 +1,6 @@
-console.log('not ok # TODO implement Digest auth support in pmctl');
+require('tap').test('pmctl with Digest auth', {
+  todo: 'implement Digest auth support in pmctl'
+});
 
 // TODO: In order to use Digest auth across the board, the pmctl CLI must
 // support Digest auth for all actions. Currently actions that involve
