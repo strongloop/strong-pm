165cbff850ea52b484ab826441384a0abc6f57d6
Ryan Graham <ryan@strongloop.com>
2015-05-14 18:07:54 -0700

    test: simplify log-dump tests
    Using a static string that is printed immiately on startup should be
    more resilient to overloaded CI machines.

--- test/test-pmctl-ssh2.js
+++ test/test-pmctl-ssh2.js
@@ -9,7 +9,10 @@ tap.test('pmctl over ssh', function(t) {
     var sshPath = fmt('http+ssh://127.0.0.1:%d', pm.port);
     var pmctl = helper.pmctlWithCtl(sshPath);
     t = helper.queued(t);
-    t.waiton(pmctl('status', '1'), 'Processes');
+    // worker 1 will show up in the status as something like
+    //      1.1.6   6    1     0.0.0.0:3000
+    var wid1 = /1\.1\.\d+\s+\d+\s+1\s+ \d+\.\d+\.\d+\.\d+:\d+/g;
+    t.waiton(pmctl('status', '1'), wid1);
     t.expect(pmctl('log-dump', '1'), /.+ worker:1 pid \d+ listening on \d+/);
     t.shutdown(pm);
   });
