900f20cb365875651ece523515f22865bbd286db
Ryan Graham <ryan@strongloop.com>
2015-05-19 19:25:11 -0700

    test: test docker driver startup

--- test/test-docker-driver.js
+++ test/test-docker-driver.js
@@ -1,8 +1,16 @@
 'use strict';
 
+var debug = require('debug')('strong-pm:test');
 var DockerDriver = require('../lib/drivers/docker');
 var driverHelpers = require('./driver-helpers');
+var EE = require('events').EventEmitter;
+var inherits = require('util').inherits;
 var tap = require('tap');
+
+var debugConsole = {
+  log: debug,
+  error: debug,
+};
 
 tap.test('DockerDriver constructor API', function(t) {
   driverHelpers.testConstructor(t, DockerDriver);
@@ -21,4 +29,69 @@ tap.test('Docker containers', function(t) {
   t.assert('startOpts' in instance, 'instnace has startOpts');
   t.assert('log' in instance, 'instance has log buffer');
   t.end();
+});
+
+tap.test('DockerDriver#start', function(t) {
+  var pulledImages = [];
+  var gotInfo = false;
+  var listeners = [];
+  var images = [];
+  var commits = [];
+  var mockDocker = {
+    info: function(cb) {
+      gotInfo = true;
+      cb(null, {NCPUS: 2});
+    },
+    pull: function(name, opts, cb) {
+      pulledImages.push(name);
+      cb(null);
+    },
+    modem: {
+      followProgress: function(_str, cb) { cb(); },
+    },
+    getImage: function(name) {
+      return {
+        inspect: function(cb) {
+          cb(null, {});
+        },
+      };
+    },
+  };
+  function MockImage(driver, id, baseDir) {
+    EE.call(this);
+    images.push(id);
+    this.on('commit', function(c) {
+      commits.push(c);
+    });
+    this.on('newListener', function(e) {
+      listeners.push(e);
+    });
+  }
+  inherits(MockImage, EE);
+  var docker = new DockerDriver({
+    baseDir: 'BASE',
+    console: debugConsole,
+    server: {},
+    docker: mockDocker,
+    Image: MockImage,
+  });
+
+  var metas = {
+    '1': {
+      commit: {
+        hash: 'abcdef123456',
+        dir: __dirname,
+      },
+    },
+  };
+
+  docker.start(metas, function(err) {
+    t.ifError(err, 'should not error on startup');
+    t.equal(pulledImages.length, 2, 'attempted to pull 2 base images');
+    t.assert(gotInfo, 'polled docker server info on startup');
+    t.deepEqual(images, [1], 'creates image for instnace 1');
+    t.deepEqual(listeners, ['error', 'image'], 'driver listens for image');
+    t.deepEqual(commits, [metas['1'].commit], 'Image for instnace 1 commit');
+    t.end();
+  });
 });
