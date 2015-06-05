395eb5b86ecfc0d1dbb4b7a371b148ee0841ac37
Sam Roberts <sam@strongloop.com>
2015-06-05 08:51:58 -0700

    test: use tap and the cli, not async/internal apis

--- test/test-usage.js
+++ test/test-usage.js
@@ -1,68 +1,52 @@
-var assert = require('assert');
-var async = require('async');
+'use strict';
+
+var concat = require('concat-stream');
 var debug = require('debug')('strong-pm:test');
+var executor = require.resolve('../bin/sl-pm.js');
+var fork = require('child_process').fork;
 var path = require('path');
 var tap = require('tap');
 
-require('shelljs/global');
+tap.test('version,--version', function(t) {
+  var version = require('../package.json').version;
 
-console.log('working dir for %s is %s', process.argv[1], process.cwd());
-
-// Prevent usage text from confusing the TAP parser
-console.log = console.error;
-
-var main = require('../').main;
-
-// Check for node silently exiting with code 0 when tests have not passed.
-var ok = false;
-
-process.on('exit', function(code) {
-  if (code === 0) {
-    assert(ok);
-  }
-});
-
-function expectError(er) {
-  if(er) {
-    return null;
-  } else {
-    return Error('expected error');
-  }
-}
-
-// argv [0] and [1] are ignored (they are node and script name, not options)
-var tests = [
-  main.bind(null, ['', '', '-h']),
-  main.bind(null, ['', '', '--help']),
-  main.bind(null, ['', '', '-hv']),
-  main.bind(null, ['', '', '-v']),
-  main.bind(null, ['', '', '--version']),
-  main.bind(null, ['', '', '-vh']),
-  function(callback) {
-    main(['', '', 'no-such-arg'], function(er) {
-      return callback(expectError(er));
-    });
-  },
-  function(callback) {
-    main(['', '', '--no-such-option'], function(er) {
-      return callback(expectError(er));
-    });
-  },
-  function(callback) {
-    main(['', '', '-Z'], function(er) {
-      return callback(expectError(er));
-    });
-  },
-];
-
-tap.test('sl-pm usage', function(t) {
-  t.plan(tests.length + 1);
-  async.parallel(tests, function(err, results) {
-    results.forEach(function(res) {
-      t.ifError(res);
-    });
-    t.ifError(err);
-    ok = true;
+  cli('--version').stdout.pipe(concat({encoding: 'string'}, function(line) {
+    debug('line <%s>', line);
+    t.equal(line.trim(), version);
     t.end();
-  });
+  }));
 });
+
+tap.test('version,-v', function(t) {
+  var version = require('../package.json').version;
+
+  cli('-v').stdout.pipe(concat({encoding: 'string'}, function(line) {
+    debug('line <%s>', line);
+    t.equal(line.trim(), version);
+    t.end();
+  }));
+});
+
+tap.test('help,-h', function(t) {
+  cli('-h').stdout.pipe(concat({encoding: 'string'}, function(line) {
+    debug('line <%s>', line);
+    t.assert(/usage:/.test(line));
+    t.end();
+  }));
+});
+
+tap.test('help,--help', function(t) {
+  cli('--help').stdout.pipe(concat({encoding: 'string'}, function(line) {
+    debug('line <%s>', line);
+    t.assert(/usage:/.test(line));
+    t.end();
+  }));
+});
+
+function cli() {
+  var child = fork(executor, [].slice.call(arguments), {silent: true});
+
+  child.stderr.pipe(process.stderr);
+
+  return child;
+}
