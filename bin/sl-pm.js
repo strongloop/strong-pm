d684bd1e653d8f7d865e9342de443009740a2d0d
Krishna Raman <kraman@gmail.com>
2016-01-11 07:47:20 -0800

    Revert name change of JSON db file

--- bin/sl-pm.js
+++ bin/sl-pm.js
@@ -205,7 +205,7 @@ function stopWhenDone(/* $0, app */) {
 
 function checkAndUpgradeDb(baseDir, callback) {
   var memoryDbLocation = process.env.STRONGLOOP_MESH_DB ||
-    path.join(baseDir, 'strong-mesh.json');
+    path.join(baseDir, 'strong-pm.json');
   fs.stat(memoryDbLocation, function(err) {
     if (err) return callback();
     upgradeDb(baseDir, memoryDbLocation, false, callback);
